73e276836e22d2cb11e1a16cd40142384c770273
JasonZV94 <jazuva28@gmail.com>
2020-02-06 18:22:15 -0600

    tarea

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 frutas = ["Manzana","Pera","Banano","Uva"]
 
+matrix = [["A","B","C","D"], ["E","F","G","H"], ["I","J","K","L"] , ["M","N","O","P"], ["Q","R","S","T"] ]
+
+respuesta = ""
+
+for row in matrix:
+    print(respuesta)
+    respuesta = ""
+    for colum in row:
+        respuesta = respuesta + " " + colum
+
+
+
 for fruta in frutas:
     if fruta == "Pera":
         print("Si hay coincidencias")
